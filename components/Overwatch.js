719859ed6f020fa888d03aca6eca36a91fadbc52
jaydeerasmo <jaydeerasmo@gmail.com>
2019-05-13 16:22:36 -0500

    Overwatch Class

---
+++ components/Overwatch.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const url = "https://ovrstat.com/stats";
+
+export function getProfile(platform, name){
+  const endpoint = `${url}/${platform}/${name}`
+  return fetch(endpoint).then((res) => res.json());
+}
