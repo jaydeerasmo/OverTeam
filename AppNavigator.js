c3a9d412074b28659a4eedaa2fd30a9f7991deab
jaydeerasmo <jaydeerasmo@gmail.com>
2019-05-14 16:02:27 -0500

    NewTeamScreen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import StartScreen from './screens/StartScreen';
+import NewTeamScreen from './screens/NewTeamScreen';
 
-const AppStack = createStackNavigator({Start: StartScreen});
+const AppStack = createStackNavigator({Start: StartScreen, NewTeam: NewTeamScreen });
 
 export default createAppContainer(createSwitchNavigator({
     App: AppStack
---
+++ screens/NewTeamScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default class NewTeamScreen extends React.Component {
+  render(){
+    return(
+      <View style={styles.container}>
+
+      </View>
+
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
