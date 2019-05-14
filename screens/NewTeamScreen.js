c42f0924592e430a350e6d455f12d6ca9c548e61
jaydeerasmo <jaydeerasmo@gmail.com>
2019-05-14 16:17:47 -0500

    NewTeamScreen

--- screens/NewTeamScreen.js
+++ screens/NewTeamScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Picker} from 'react-native';
+import * as Colors from '../constants/Colors';
 
 export default class NewTeamScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      platform: 'Select Platform',
+      team: [],
+    };
+  }
   render(){
     return(
       <View style={styles.container}>
+        <Picker
+          selectedValue={this.state.platform}
+          style = {styles.picker}
+          onValueChange = {(itemValue, itemIndex) => this.setState({platform: itemValue})}
+        >
+          <Picker.Item label="Xbox" value="xbl"/>
+          <Picker.Item label="PC" value="pc"/>
+          <Picker.Item label="Playstation" value="psn"/>
+        </Picker>
 
       </View>
 
@@ -14,6 +33,6 @@ export default class NewTeamScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.DARK_GREY,
   }
 });
